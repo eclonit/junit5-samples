bb03d3503b54d739633a225b05aafda7ec60e30f
eclonit <davizinhozl.mendes@gmail.com>
2019-08-17 11:39:53 -0300

    atualização 1

--- junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -19,11 +19,31 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
+	Calculator calculator = new Calculator();
+
 	@Test
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+		assertEquals(2, calculator.add(1, 1), "1 + 1 deveria ser igual a 2");
+	}
+	
+	@Test
+	@DisplayName("1 - 1 = 0")
+	void subTwoNumbers(){
+		assertEquals(0, calculator.sub(1, 1), "1 - 1 deveria ser igual a 0");
+	}
+	
+	@Test
+	@DisplayName("5 * 5 = 25")
+	void multTwoNumbers(){
+		assertEquals(25, calculator.mult(5, 5), "5 * 5 deveria ser igual a 25");
+	}
+	
+	@Test
+	@DisplayName("25 / 5 = 5")
+	void divTwoNumbers(){
+		assertEquals(5, calculator.div(25, 5), "25 / 5 deveria ser igual a 5");
+		assertEquals(0, calculator.div(25, 0), "25 / 0 deveria retornar zero ou nulo");
 	}
 
 	@ParameterizedTest(name = "{0} + {1} = {2}")
